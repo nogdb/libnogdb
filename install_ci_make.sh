aa1dc1a5b18a3b058c926fadec3190f8a7adde9c
Peez Gloriousintel <apzku69.tutor@gmail.com>
2018-07-30 22:49:20 +0700

    Update LICENSE file and headers

--- install_ci_make.sh
+++ install_ci_make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp Makefile.bak Makefile
-cp lib/lmdb/Makefile.bak lib/lmdb/Makefile
+cp ci/Makefile Makefile
+cp ci/lmdb/Makefile lib/lmdb/Makefile
 
-echo "installing Makefile... [Done]"
+echo "installing Makefile for CI... [Done]"
